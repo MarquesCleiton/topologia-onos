7240e52b1fa3a7ad28874674c97b61622372f56b
marquescleiton <cleiton.contato2210@gmail.com>
2020-10-20 22:12:51 -0300

    atulaizaçao de pom4

--- simple-firewall/src/main/java/br/com/usjt/simplefirewall/MaisUmTeste.java
+++ simple-firewall/src/main/java/br/com/usjt/simplefirewall/MaisUmTeste.java
@@ -7,10 +7,6 @@ import org.onosproject.cli.AbstractShellCommand;
 @Command(scope = "onos", name = "meu-teste", description = "meu teste")
 public class MaisUmTeste extends AbstractShellCommand{
 	
-	@Argument(index = 0, name = "argEvcCfgId",
-            description = "EVC configuration ID", required = true, multiValued = false)
-    String argEvcCfgId = null;
-	@Override
 	protected void execute() {
 		print("testando a minha aplicação");
 	}
