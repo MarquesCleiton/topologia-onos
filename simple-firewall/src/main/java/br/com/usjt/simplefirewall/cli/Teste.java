3d22ded7258ddf89ce4ed53f895499ba33e8772d
marquescleiton <cleiton.contato2210@gmail.com>
2020-10-20 21:54:54 -0300

    atulaizaçao de pom2

--- simple-firewall/src/main/java/br/com/usjt/simplefirewall/cli/Teste.java
+++ simple-firewall/src/main/java/br/com/usjt/simplefirewall/cli/Teste.java
@@ -3,7 +3,7 @@ package br.com.usjt.simplefirewall.cli;
 import org.apache.karaf.shell.commands.Command;
 import org.onosproject.cli.AbstractShellCommand;
 
-@Command(scope = "Teste", name = "teste-teste",
+@Command(scope = "onos", name = "teste-teste",
 description = "meu teste")
 public class Teste extends AbstractShellCommand{
 
