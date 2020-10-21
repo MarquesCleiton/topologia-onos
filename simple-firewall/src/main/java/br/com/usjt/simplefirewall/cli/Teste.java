ca202bbc20547b1494422dd4d959cc4039a13ba2
marquescleiton <cleiton.contato2210@gmail.com>
2020-10-20 21:42:47 -0300

    teste aplicacao

---
+++ simple-firewall/src/main/java/br/com/usjt/simplefirewall/cli/Teste.java
@@ -0,0 +1,15 @@
+package br.com.usjt.simplefirewall.cli;
+
+import org.apache.karaf.shell.commands.Command;
+import org.onosproject.cli.AbstractShellCommand;
+
+@Command(scope = "Teste", name = "teste-teste",
+description = "meu teste")
+public class Teste extends AbstractShellCommand{
+
+	@Override
+	protected void execute() {
+		
+	}
+
+}
